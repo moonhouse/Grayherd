d8792ab05739200b6b156fc0709bd34e069468ad
David Hall <david.hall@tv4.se>
2012-05-15 18:54:53 +0200

    Split i18n to individual files.

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Grayherd
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
      config.i18n.default_locale = :sv
      config.i18n.locale = :sv
 
