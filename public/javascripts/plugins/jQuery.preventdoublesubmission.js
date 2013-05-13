0d5bde9f48ecbf05067da259b3c4fa21b47e9fdf
David Hall <david.hall@tv4.se>
2013-05-13 19:23:47 +0200

    Prevent double form submission on client side.

---
+++ public/javascripts/plugins/jQuery.preventdoublesubmission.js
@@ -0,0 +1,16 @@
+jQuery.fn.preventDoubleSubmission = function() {
+    $(this).bind('submit',function(e){
+        var $form = $(this);
+
+        if ($form.data('submitted') === true) {
+            // Previously submitted - don't submit again
+            e.preventDefault();
+        } else {
+            // Mark it so that the next submit can be ignored
+            $form.data('submitted', true);
+        }
+    });
+
+    // Keep chainability
+    return this;
+};
